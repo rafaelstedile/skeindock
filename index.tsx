707a46216c0194fbfc7e12f8f948519cb694bd2f
rafaelstedile <rafaelstedile@gmail.com>
2026-01-09 14:22:32 -0300

    Remove optional CSS import from index.tsx

--- index.tsx
+++ index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Opcional, caso crie estilos globais
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
